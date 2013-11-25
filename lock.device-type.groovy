3c8a8405e26f982e90545c35d760246e34896a6a
bigpunk6 <mkurtzjr@live.com>
2013-11-25 15:23:35 -0500

    Update lock.device-type.groovy

--- lock.device-type.groovy
+++ lock.device-type.groovy
@@ -1,3 +1,22 @@
+/** 
+ * This is a custom version of the Z-Wave Lock Device Type code. This code adds support for user codes with the use of a Smartapp.
+ *
+ * This Device Type was designed for the Kwikset Deadbolt so might not be compatible with other locks.
+ *
+ * Installation
+ *
+ * Create a new device type (https://graph.api.smartthings.com/ide/devices)
+ *    Capabilities:
+ *        Configuration
+ *        Battery
+ *        Polling
+ *        Lock
+ *    Custom Attribute
+ *        user1
+ *        code1
+ *    Custom Command
+ *        usercodechange
+ */
 metadata {
 	simulator {
 		status "locked": "command: 9881, payload: 00 62 03 FF 00 00 FE FE"
